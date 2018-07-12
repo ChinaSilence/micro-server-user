ff0c8924cd1a411a0689644b49398ef14746278a
anoy <545544032@qq.com>
2018-07-12 17:01:26 +0800

    add application name

---
+++ src/main/java/com/anoyi/user/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.anoyi.user.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @RequestMapping("/")
+    public String home(){
+        return this.applicationName;
+    }
+
+}
