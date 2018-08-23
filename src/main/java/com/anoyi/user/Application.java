a14358467f01df83dbb393b0f6bd3124d02140f7
anoy <545544032@qq.com>
2018-08-23 18:42:28 +0800

    @Id

--- src/main/java/com/anoyi/user/Application.java
+++ src/main/java/com/anoyi/user/Application.java
@@ -1,10 +1,12 @@
 package com.anoyi.user;
 
 import com.anoyi.BootApplication;
+import com.anoyi.grpc.annotation.GrpcServiceScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@GrpcServiceScan(basePackages = "com.anoyi.server.rpc.user")
 public class Application extends BootApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/anoyi/user/entity/User.java
+++ src/main/java/com/anoyi/user/entity/User.java
@@ -1,11 +1,11 @@
 package com.anoyi.user.entity;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
